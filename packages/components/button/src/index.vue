8ba0edcb1626a4a017388c982b594eb63053bdea
chris-zhu <1633711653@qq.com>
2022-08-13 15:46:40 +0800

    fix(components): improve `button` props

--- packages/components/button/src/index.vue
+++ packages/components/button/src/index.vue
@@ -2,31 +2,21 @@
  interface IButtonProps {
   to?: string
   icon?: string
-  type?: 'primary' | 'success' | 'warning' | 'danger' | 'info' | ''
+  light?: boolean
 }
 
 withDefaults(defineProps<IButtonProps>(), {
-  to: 'circle',
+  to: '',
   icon: 'medium',
-  type: '',
 })
 </script>
 
 <template>
   <component
     :is="to ? 'a' : 'button'"
-    :href="to"
-    :class="[
-      type === 'primary' && 'bg-[#409eff] text-white o-borderless active:bg-[#337ecc] hover:bg-[#79bbff] hover:text-white hover:o-borderless',
-      type === 'success' && 'bg-[#67C23A] text-white o-borderless active:bg-[#337ecc] hover:bg-[#95d475] hover:text-white hover:o-borderless',
-      type === 'info' && 'bg-[#909399] text-white o-borderless active:bg-[#73767a] hover:bg-[#b1b3b8] hover:text-white hover:o-borderless',
-      type === 'warning' && 'bg-[#e6a23c] text-white o-borderless active:bg-[#b88230] hover:bg-[#eebe77] hover:text-white hover:o-borderless',
-      type === 'danger' && 'bg-[#f56c6c] text-white o-borderless active:bg-[#c45656] hover:bg-[#f89898] hover:text-white hover:o-borderless',
-      type === 'danger' && 'bg-[#f56c6c] text-white o-borderless active:bg-[#c45656] hover:bg-[#f89898] hover:text-white hover:o-borderless',
-      type === '' && '',
-      to && 'o-button-link',
-    ]"
-    class="o-transition o-button-base hover:o-button-hover active:o-button-active focus-visible:o-focus-base o-disabled:o-disabled inline-flex items-center justify-center"
+    v-bind="to ? { href: to } : {}"
+    class="o-transition o-button-base"
+    :class="light ? 'o-button-light' : ''"
   >
     <slot name="icon">
       <!-- <NIcon v-if="icon" :icon="icon" class="c-buttoc-icon" /> -->
--- playground/components/TheButton.vue
+++ playground/components/TheButton.vue
@@ -1,32 +1,58 @@
 <script lang="ts" setup>
-const githubUrl = 'https://github.com/chris-zhu'
+const githubUrl = 'https://github.com/onu-ui/onu-ui'
 </script>
 
 <template>
   <Card name="Button">
-    <OButton>
-      Default
-    </OButton>
-    <OButton type="primary">
-      Primary
-    </OButton>
-    <OButton type="success">
-      Success
-    </OButton>
-    <OButton type="warning">
-      Warning
-    </OButton>
-    <OButton type="danger">
-      Danger
-    </OButton>
-    <OButton type="info">
-      Info
-    </OButton>
-    <o-button :to="githubUrl">
-      <template #icon>
-        <div i-carbon-logo-github />
-      </template>
-      github
-    </o-button>
+    <div f-c-c gap-2>
+      <div fcc gap-2>
+        <OButton o="primary">
+          Primary
+        </OButton>
+        <OButton o="secondary">
+          Secondary
+        </OButton>
+        <OButton o="success">
+          Success
+        </OButton>
+        <OButton o="warning">
+          Warning
+        </OButton>
+        <OButton o="error">
+          Danger
+        </OButton>
+        <OButton o="info">
+          Info
+        </OButton>
+      </div>
+      <div fcc gap-2>
+        <OButton o="primary light">
+          Primary
+        </OButton>
+        <OButton light o="secondary">
+          Secondary
+        </OButton>
+        <OButton light o="success">
+          Success
+        </OButton>
+        <OButton light o="warning">
+          Warning
+        </OButton>
+        <OButton light o="error">
+          Danger
+        </OButton>
+        <OButton light o="info">
+          Info
+        </OButton>
+      </div>
+      <div fcc gap-2>
+        <o-button :to="githubUrl">
+          <template #icon>
+            <div i-carbon-logo-github />
+          </template>
+          github
+        </o-button>
+      </div>
+    </div>
   </Card>
 </template>
