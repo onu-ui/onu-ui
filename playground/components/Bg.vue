1f9c6e48a9e06c0f9746f33ed59ef59c2939f2e2
Chris <1633711653@qq.com>
2022-08-13 16:02:13 +0800

    feat(components & preset): make sure Theme color & improve `button` props (#8)
    * chore: update
    
    * feat(preset): mark sure theme color
    
    * fix(components): improve `button` props
    
    * chore: lint fix
    
    * chore: lint fix

--- playground/components/Bg.vue
+++ playground/components/Bg.vue
@@ -1,10 +1,9 @@
-<script lang='ts' setup>
-
-</script>
-
 <template>
   <div class="bg" />
 </template>
 
 <style lang='scss' scoped>
+.bg {
+    @apply fixed top-0 left-0 right-0 bottom-0 z--1 backdrop-blur-190px;
+}
 </style>
