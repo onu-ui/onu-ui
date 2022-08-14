885d5a6fcb1fb51088312efe92c81f0ad81bb55a
chris-zhu <1633711653@qq.com>
2022-08-14 15:10:17 +0800

    fix(playground): update header

--- playground/components/Layout.vue
+++ playground/components/Layout.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="w-65% ma space-y-8">
-    <h1>Playground</h1>
+  <header mb4 h-16 fbc px-4 b="b dashed gray3">
+    <div fsc gap-4>
+      <img w-8 h-8 src="/logo.svg" alt="">
+      <h1 text="primary white">Playground</h1>
+    </div>
+    <o-button text o="primary" to="https://github.com/onu-ui/onu-ui">
+      <template #icon>
+        <div i-carbon-logo-github></div>
+      </template>
+      Github
+    </o-button>
+  </header>
+  <main class="w-65% ma space-y-8">
     <slot />
-  </div>
+  </main>
 </template>
