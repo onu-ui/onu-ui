70bd27017c6d46749d039151d8ad9b79d7798661
chris-zhu <1633711653@qq.com>
2022-08-15 16:51:51 +0800

    chore: update

--- packages/components/button/src/index.vue
+++ packages/components/button/src/index.vue
@@ -18,13 +18,19 @@ const isDisabled = computed(() => props.loading || props.disabled)
 </script>
 
 <template>
-  <component :is="to ? 'a' : 'button'" v-bind="to && { href: to }" :disabled="isDisabled"
-    class="o-transition o-button-base o-button-hover o-button-active" :class="[
+  <component
+    :is="to ? 'a' : 'button'"
+    v-bind="to && { href: to }"
+    :disabled="isDisabled"
+    :aria-disabled="isDisabled"
+    class="o-button-base"
+    :class="[
       light ? 'o-button-light' : '',
       text ? 'o-button-text' : '',
       `o-button-${size}`,
-      isDisabled ? 'o-disabled' : '',
-    ]">
+      isDisabled ? 'o-disabled' : 'o-transition o-button-hover o-button-active',
+    ]"
+  >
     <div v-if="loading" i-carbon-circle-dash animate-spin />
     <slot v-else name="icon">
       <!-- <NIcon v-if="icon" :icon="icon" class="c-buttoc-icon" /> -->
