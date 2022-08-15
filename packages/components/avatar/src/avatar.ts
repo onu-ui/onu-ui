00ed3aac7972855ed7d22dd5cc3adb2c0017b979
IceyWu <3128006406@qq.com>
2022-08-15 22:19:34 +0800

    feat(components): add avatar feature

---
+++ packages/components/avatar/src/avatar.ts
@@ -0,0 +1,5 @@
+
+export const avatarEmits = {
+  error: (evt: Event) => evt instanceof Event,
+}
+export type AvatarEmits = typeof avatarEmits
--- packages/components/avatar/src/index.vue
+++ packages/components/avatar/src/index.vue
@@ -1,19 +1,59 @@
 <script lang='ts' setup name="O-Avatar">
+import { avatarEmits } from './avatar'
 interface IAvatarProps {
-  src: string
-  shape: 'square' | 'circle'
-  size: 'small' | 'medium' | 'large'
+  src?: string
+  icon?: string
+  shape?: 'square' | 'circle'
+  size?: 'small' | 'default' | 'large'
+  fit?:'fill' | 'contain' | 'cover' | 'none' | 'scaleDown'
+  alt?: string
+  srcSet?: string
 }
 
-withDefaults(defineProps<IAvatarProps>(), {
+const props = withDefaults(defineProps<IAvatarProps>(), {
   shape: 'circle',
-  size: 'medium',
+  size: 'default',
+  fit: 'cover',
 })
+const emit = defineEmits(avatarEmits)
+const flag = true
+const handleError = (e: Event) => {
+  emit('error', e)
+}
+const sizeEnum = {
+  small: 'o-avatar-size-small',
+  default: 'o-avatar-size-default',
+  large: 'o-avatar-size-large',
+}
+const fitEnum = {
+  fill: 'o-avatar-fit-fill',
+  contain: 'o-avatar-fit-contain',
+  cover: 'o-avatar-fit-cover',
+  none: 'o-avatar-fit-none',
+  scaleDown: 'o-avatar-fit-scale-down',
+}
+const shapeEnum = {
+  square: 'rounded-md',
+  circle: 'rounded-full',
+}
 </script>
 
 <template>
   <div>
-    <div w-20 h-20 :style="{ backgroundImage: `url(${src})` }" :class="shape === 'circle' ? 'rounded-full' : 'rounded-md'" class="bg-cover" />
+    <div :class="[`${sizeEnum[size]}`, `${shapeEnum[shape]}`]" class="o-avatar-base">
+      <img
+        v-if="src || srcSet"
+        :src="src"
+        :alt="alt"
+        :src-set="srcSet"
+        class="block h-full w-full"
+        :class="[`${fitEnum[fit]}`]"
+        @error="handleError"
+      >
+
+      <div v-else-if="icon" :class="icon" class="o-avatar-icon" />
+      <slot v-else />
+    </div>
   </div>
 </template>
 
