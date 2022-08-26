6a1b1763b2454a5f92d924a5feaca6d5bd5e6020
mohen <251205668@qq.com>
2022-08-26 20:42:14 +0800

    style: reset o-card-base

--- playground/src/components/base-card.vue
+++ playground/src/components/base-card.vue
@@ -8,7 +8,7 @@ defineProps<ICardProps>()
 
 <template>
   <div
-    class="rounded-lg shadow-lg p-8 bg-gradient-from-rgba(255, 255, 255, 0.2) bg-gradient-to-rgba(255, 255, 255, 0.035) backdrop-blur-lg overflow-hidden"
+    class="o-card-base"
   >
     <slot />
   </div>
