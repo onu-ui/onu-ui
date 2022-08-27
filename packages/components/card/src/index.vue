542b08a337363b86fac97363c8b4899a7476b452
chris-zhu <1633711653@qq.com>
2022-08-27 18:58:22 +0800

    chore: update

--- packages/components/card/src/index.vue
+++ packages/components/card/src/index.vue
@@ -1,9 +1,32 @@
 <script lang="ts" setup name="O-Card">
 import OButton from '../../button/src/index.vue'
-import type { ICardProps } from './type'
+import type { INode, SizeType } from '../../types'
+
+interface ICardProps {
+  /** 卡片标题 */
+  title?: string
+  /** 描述 */
+  description?: string
+  /** 内容 */
+  content?: string | INode;
+  /** 封面 */
+  cover?: string;
+  /** 卡片大小，支持 md，sm */
+  size?: Exclude<SizeType, 'xs' | 'lg'>;
+  /** 悬浮阴影 */
+  hoverable?: boolean;
+  /** 边框 */
+  bordered?: boolean;
+  /** 头部内容区 */
+  header?: string | INode;
+   /** 卡片右上角操作区 */
+  extra?: string | INode;
+  /** 底部操作区 */
+  actions?: string | INode;
+}
 
 const props = withDefaults(defineProps<ICardProps>(), {
-
+  // eslint-disable-next-line vue/require-valid-default-prop
   size: 'md',
 })
 
