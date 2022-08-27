d28650636d5030d5212c5154feb66f6df5c60f03
baiwusanyu <740132583@qq.com>
2022-08-27 20:43:18 +0800

    docs: add o-checkbox document

---
+++ docs/example/checkbox/indeterminate.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+const checkVal = ref(true)
+</script>
+
+<template>
+  <o-checkbox
+      indeterminate
+      v-model="checkVal"
+      label="indeterminate"
+  />
+</template>
