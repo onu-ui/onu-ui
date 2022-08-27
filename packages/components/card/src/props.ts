a6d8148533bf1353826486443ee437d7aea503ab
chris-zhu <1633711653@qq.com>
2022-08-27 18:55:11 +0800

    fix: remove deps & update props type

--- packages/components/card/src/props.ts
+++ packages/components/card/src/props.ts
@@ -2,13 +2,21 @@ import type { PropType } from 'vue'
 import type { ICardProps } from './type'
 
 export default {
-  /** 卡片操作区 */
-  actions: {
-    type: [String, Function] as PropType<ICardProps['actions']>,
-  },
   /** 卡片标题 */
   title: {
     type: [String, Function] as PropType<ICardProps['title']>,
+  },
+  /** 描述 */
+  description: {
+    type: [String] as PropType<ICardProps['description']>,
+  },
+  /** 内容 */
+  content: {
+    type: [String, Function] as PropType<ICardProps['content']>,
+  },
+  /** 封面 */
+  cover: {
+    type: [String] as PropType<ICardProps['content']>,
   },
   /** 卡片大小，支持 md，sm */
   size: {
@@ -19,35 +27,20 @@ export default {
       return ['md', 'sm'].includes(val)
     },
   },
-  /** 副标题 */
-  subtitle: {
-    type: [String, Function] as PropType<ICardProps['subtitle']>,
-  },
   /** 悬浮阴影 */
-  hoverShadow: Boolean,
-  /** 阴影 */
-  shadow: Boolean,
+  hoverable: Boolean,
   /** 边框 */
-  bordered: {
-    type: Boolean,
-    default: true,
-  },
-  /** 内容 */
-  content: {
-    type: [String, Function] as PropType<ICardProps['content']>,
-  },
-  /** 描述 */
-  description: {
-    type: [String, Function] as PropType<ICardProps['description']>,
-  },
+  bordered: Boolean,
   /** 头部内容区 */
   header: {
+    type: [Function] as PropType<ICardProps['header']>,
+  },
+  /** 卡片右上角操作区 */
+  extra: {
     type: [String, Function] as PropType<ICardProps['header']>,
   },
-  /** 底部内容区 */
-  footer: {
-    type: [String, Function] as PropType<ICardProps['footer']>,
+  /** 卡片操作区 */
+  actions: {
+    type: [Function] as PropType<ICardProps['actions']>,
   },
-  /** 头部和内容区是否展示分割线 */
-  headerBordered: Boolean,
 }
--- packages/components/card/src/type.ts
+++ packages/components/card/src/type.ts
@@ -1,28 +1,24 @@
 import type { INode, SizeType } from '../../types'
 
 export interface ICardProps {
-  /** 卡片操作区 */
-  actions?: string | INode
   /** 卡片标题 */
-  title?: string | INode
-  /** 卡片大小，支持 md，sm */
-  size?: Exclude<SizeType, 'xs' | 'lg'>
-  /** 副标题 */
-  subtitle?: string | INode
-  /** 悬浮阴影 */
-  hoverShadow?: boolean
-  /** 阴影 */
-  shadow?: boolean
-  /** 边框 */
-  bordered?: boolean
+  title?: string
+  /** 描述 */
+  description?: string
   /** 内容 */
   content?: string | INode
-  /** 描述 */
-  description?: string | INode
+  /** 封面 */
+  cover?: string
+  /** 卡片大小，支持 md，sm */
+  size?: Exclude<SizeType, 'xs' | 'lg'>
+  /** 悬浮阴影 */
+  hoverable?: boolean
+  /** 边框 */
+  bordered?: boolean
   /** 头部内容区 */
-  header?: string | INode
-  /** 底部内容区 */
-  footer?: string | INode
-  /** 头部和内容区是否展示分割线 */
-  headerBordered?: boolean
+  header?: INode
+  /** 卡片右上角操作区 */
+  extra?: string | INode
+  /** 底部操作区 */
+  actions?: INode
 }
