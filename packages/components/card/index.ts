23b1152ea1f61824b66739595b52331e9fa73aea
mohen <Y251205668@163.com>
2022-08-29 14:21:41 +0800

    perf(components): remove OCard redundant files,unified export OCardProps and OCardInstance (#29)
    * perf: remove OCard redundant files,unified export OCardProps and OCardInstance
    
    * style: lint card shortcuts
    
    Co-authored-by: chris-zhu <1633711653@qq.com>

--- packages/components/card/index.ts
+++ packages/components/card/index.ts
@@ -1,7 +1,7 @@
 import { withInstall } from '@onu-ui/utils'
 import Card from './src/index.vue'
-import type { ICardProps } from './src/type'
+
+export * from './src/props'
 
 export const OCard = withInstall(Card)
-export type OCardProps = ICardProps
 export default OCard
