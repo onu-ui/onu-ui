9ec9b1cb52e841b64cb72a8f6b56e2e3fa1415e2
baiwusanyu <740132583@qq.com>
2022-09-18 23:25:56 +0800

    feat: improve rate component

--- packages/components/rate/src/index.vue
+++ packages/components/rate/src/index.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup name="O-Rate">
-import { computed } from 'vue'
 import OIcon from '../../icon/src/index.vue'
 import { rateProps } from './props'
 
 const props = defineProps(rateProps)
 const emits = defineEmits(['update:modelValue', 'change'])
 
-const curVal = ref(props.modelValue)
+const modelVal = computed(() => props.modelValue)
+const curVal = ref(modelVal.value)
 const isAtLeftHalf = ref(false)
 const setCurValue = (item:number, e:MouseEvent) => {
   if (props.readonly) return
@@ -21,11 +21,6 @@ const resetCurValue = () => {
   if (props.readonly) return
   curVal.value = props.modelValue
 }
-
-watch(() => props.modelValue, () => {
-  resetCurValue()
-},
-{ immediate: true })
 
 const selectVal = () => {
   if (props.readonly) return
