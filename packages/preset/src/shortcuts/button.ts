4514eec356ae47322880cc6ff0972eb785e3ffb2
yzh990918 <251205668@qq.com>
2022-09-24 16:54:37 +0800

    fix: fix outline-none not working

--- packages/preset/src/shortcuts/button.ts
+++ packages/preset/src/shortcuts/button.ts
@@ -1,11 +1,8 @@
 export const buttonShortcuts: Record<string, string> = {
   // button
-  'o-button-base':
-  'fcc gap-1 inline-flex cursor-pointer rounded shadow-context:50 !outline-none c-context o-solid border',
-  'o-button-hover':
-  'hover-border-context:64 hover-text-white hover-bg-context:64',
-  'o-button-active':
-  'active-border-context:32 active-text-white active-bg-context:32',
+  'o-button-base': 'fcc gap-1 inline-flex cursor-pointer rounded shadow-context:50 c-context o-solid border !focus:outline-none',
+  'o-button-hover': 'hover-border-context:64 hover-text-white hover-bg-context:64',
+  'o-button-active': 'active-border-context:32 active-text-white active-bg-context:32',
   'o-button-light': 'o-light',
   'o-button-text': 'o-text',
   'o-button-xs': 'px-2.5 h-6 text-xs shadow-xs',
