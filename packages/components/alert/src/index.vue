3317df749fbd462caec1a8ad2f44812f5138b742
ChengZi <ye.chen@enlighten.finance>
2022-10-21 21:30:01 +0800

    update wrong words

--- packages/components/alert/src/index.vue
+++ packages/components/alert/src/index.vue
@@ -8,7 +8,7 @@ const slots = useSlots()
 
 const visible = ref(true)
 
-const handleColose = (evt: MouseEvent) => {
+const handleClose = (evt: MouseEvent) => {
   visible.value = false
   emit('close', evt)
 }
@@ -33,7 +33,7 @@ const showTitle = computed(() => props.title || slots.title)
             {{ title }}
           </slot>
         </div>
-        <div v-if="closable" class="o-alert-close" @click="handleColose">
+        <div v-if="closable" class="o-alert-close" @click="handleClose">
           <slot name="close">
             <o-icon v-if="!closeText" name="i-carbon-close" class="!c-white text-4" />
             <span class="text-xs">{{ closeText }}</span>
