3ece621391ebfea288bd4185e51ca12554887d60
mohen <Y251205668@163.com>
2022-11-09 21:45:38 +0800

    fix(onu-ui): change tsup to vite
    * fix: remove tsup build production,change to vite
    
    * chore: update
    
    * chore: update
    
    Co-authored-by: Chris <1633711653@qq.com>

--- packages/onu-ui/tsup.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  entry: ['src/index.ts', 'src/style.css'],
-  format: ['cjs', 'esm'],
-  minify: true,
-})
---
+++ packages/onu-ui/vite.config.ts
@@ -0,0 +1,46 @@
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import fs from 'fs-extra'
+import type { ResolvedConfig } from 'vite'
+
+let config: ResolvedConfig = undefined!
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'onu-ui',
+      fileName: 'onu-ui',
+    },
+    rollupOptions: {
+      external: [
+        'vue',
+      ],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    {
+      name: 'vite-plugin-copy-style',
+      apply: 'build',
+      enforce: 'post',
+      configResolved(_config) {
+        config = _config
+      },
+      async closeBundle() {
+        const { build, root } = config
+        const { outDir } = build
+        const styleFile = resolve(__dirname, 'src/style.css')
+        await fs.copyFile(
+          styleFile,
+          resolve(root, outDir, 'style.css'),
+        )
+      },
+    }],
+})
