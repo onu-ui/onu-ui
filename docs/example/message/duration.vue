3e6e531bad26e31cd5f6fc3238c620e48f714613
yzh990918 <251205668@qq.com>
2022-12-02 19:55:39 +0800

    docs(message): update button props

--- docs/example/message/duration.vue
+++ docs/example/message/duration.vue
@@ -12,24 +12,24 @@ function handleToClickBasic(theme:string, time:number) {
 <template>
   <div space-y-2>
     <div fscw gap-2>
-      <o-button o="primary" @click="handleToClickBasic('primary', 3000)">
+      <o-button type="primary" @click="handleToClickBasic('primary', 3000)">
         show 3.0 s
       </o-button>
-      <o-button o="secondary" @click="handleToClickBasic('secondary', 4000)">
+      <o-button type="secondary" @click="handleToClickBasic('secondary', 4000)">
         show 4.0 s
       </o-button>
-      <o-button o="info" @click="handleToClickBasic('info', 5000)">
+      <o-button type="info" @click="handleToClickBasic('info', 5000)">
         show 5.0 s
       </o-button>
     </div>
     <div fscw gap-2>
-      <o-button o="error" @click="handleToClickBasic('error', 6000)">
+      <o-button type="error" @click="handleToClickBasic('error', 6000)">
         show 6.0 s
       </o-button>
-      <o-button o="success" @click="handleToClickBasic('success', 7000)">
+      <o-button type="success" @click="handleToClickBasic('success', 7000)">
         show 7.0 s
       </o-button>
-      <o-button o="warning" @click="handleToClickBasic('warning', 8000)">
+      <o-button type="warning" @click="handleToClickBasic('warning', 8000)">
         show 8.0 s
       </o-button>
     </div>
