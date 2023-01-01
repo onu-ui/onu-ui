d9c57a90443949ca08301b4dc6bf85ecdca7d02f
Chris <1633711653@qq.com>
2023-01-02 00:21:12 +0800

    chore: change script

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,5 @@
-import { startCli } from './cli'
-import { handleError } from './error'
+// import { startCli } from './cli'
+// import { handleError } from './error'
 
-startCli().catch(handleError)
+// startCli().catch(handleError)
+export const a = 1 // TODO: remove this line
