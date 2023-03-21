9b0b89246212612cb307a03a797f513fc3eecbf9
wzc520pyfm <69044080+wzc520pyfm@users.noreply.github.com>
2023-03-21 17:18:58 +0800

    feat(component): backtop component (#141)
    * feat(component): backtop component
    
    * chore: add example for backtop component
    
    * docs: update backtop component doc

---
+++ packages/components/backtop/index.ts
@@ -0,0 +1,7 @@
+import { withInstall } from '@onu-ui/utils'
+import Backtop from './src/index.vue'
+
+export const OBacktop = withInstall(Backtop)
+export default OBacktop
+
+export * from './src/backtop'
