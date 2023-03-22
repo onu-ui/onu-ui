8559e57d4973a4419a3b978a7471180e77b78165
wzc520pyfm <1528857653@qq.com>
2023-03-22 22:56:02 +0800

    docs: add link component doc

---
+++ docs/example/link/underline.vue
@@ -0,0 +1,10 @@
+<template>
+  <div fscw gap-2>
+    <o-link type="warning">
+      with-underline
+    </o-link>
+    <o-link type="warning" :underline="false">
+      no-underline
+    </o-link>
+  </div>
+</template>
