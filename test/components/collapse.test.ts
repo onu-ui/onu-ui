7a6b35c10c730b4218f32bff310a8a613fb22501
chinbor <chinbor.mn@gmail.com>
2023-03-22 20:32:54 +0800

    bugfix: correct unit test for collapse

--- test/components/collapse.test.ts
+++ test/components/collapse.test.ts
@@ -1,7 +1,6 @@
 import { nextTick } from 'vue'
 import { describe, expect, test, vi } from 'vitest'
-import OCollapse from '@onu-ui/components/collapse/src/collapse.vue'
-import OCollapseItem from '@onu-ui/components/collapse/src/collapseItem.vue'
+import { OCollapse, OCollapseItem } from '@onu-ui/components'
 import { config, mount } from '@vue/test-utils'
 import type { OCollapseItemInstance } from '@onu-ui/components/collapse/src/props'
 import type { VueWrapper } from '@vue/test-utils'
@@ -40,15 +39,18 @@ describe('o-collapse', () => {
     expect(collapseItemWrappers[0].vm.title).toBe('title1')
     expect(collapseItemWrappers[1].vm.disabled).toBe(true)
 
-    expect(collapseItemWrappers[0].vm.isActive).toBe(true)
+    expect(collapseItemWrappers[0].getCurrentComponent().exposed!.isActive.value).toBe(true)
 
     collapseItemHeaderEls[2].click()
     await nextTick()
-    expect(collapseItemWrappers[0].vm.isActive).toBe(true)
-    expect(collapseItemWrappers[2].vm.isActive).toBe(true)
+
+    expect(collapseItemWrappers[0].getCurrentComponent().exposed!.isActive.value).toBe(true)
+    expect(collapseItemWrappers[2].getCurrentComponent().exposed!.isActive.value).toBe(true)
+
     collapseItemHeaderEls[0].click()
     await nextTick()
-    expect(collapseItemWrappers[0].vm.isActive).toBe(false)
+
+    expect(collapseItemWrappers[0].getCurrentComponent().exposed!.isActive.value).toBe(false)
   })
 
   test('o-collapse event:change', async () => {
@@ -73,6 +75,7 @@ describe('o-collapse', () => {
       },
     })
     const vm = wrapper.vm
+
     // console.log('🚀 ~ file: collapse.test.ts ~ line 84 ~ test ~ vm', wrapper.html())
     const collapseWrapper = wrapper.findComponent(OCollapse)
     const collapseItemWrappers = collapseWrapper.findAllComponents(
@@ -81,13 +84,17 @@ describe('o-collapse', () => {
     const collapseItemHeaderEls = vm.$el.querySelectorAll(
       '.o-collapse-item-title',
     )
-    expect(collapseItemWrappers[0].vm.isActive).toBe(true)
-    expect(vm.expandedNames).toEqual(['1'])
+
+    expect(collapseItemWrappers[0].getCurrentComponent().exposed!.isActive.value).toBe(true)
+
+    expect(wrapper.props().expandedNames).toEqual(['1'])
     expect(onChange).not.toHaveBeenCalled()
+
     collapseItemHeaderEls[2].click()
     await nextTick()
+
     expect(onChange).toHaveBeenCalledTimes(1)
-    expect(collapseItemWrappers[0].vm.isActive).toBe(true)
-    expect(collapseItemWrappers[2].vm.isActive).toBe(true)
+    expect(collapseItemWrappers[0].getCurrentComponent().exposed!.isActive.value).toBe(true)
+    expect(collapseItemWrappers[2].getCurrentComponent().exposed!.isActive.value).toBe(true)
   })
 })
