ec5ef9daa59b2456f27abaa50a078e349afb1b2b
wzc520pyfm <1528857653@qq.com>
2023-03-29 01:33:58 +0800

    fix(link): delete invalid class in link

--- packages/preset/src/shortcuts/link.ts
+++ packages/preset/src/shortcuts/link.ts
@@ -1,5 +1,5 @@
 export const linkShortcuts: Record<string, string> = {
   // link
-  'o-link-base': 'fcc gap-1 inline-flex row align-middle relative no-underline outline-none cursor-pointer p-0 text-baseText font-medium',
+  'o-link-base': 'fcc gap-1 inline-flex align-middle relative no-underline outline-none cursor-pointer p-0 text-baseText font-medium',
   'o-hover-link-active-base': 'hover-text-primaryBase active-text-primaryBase',
 }
