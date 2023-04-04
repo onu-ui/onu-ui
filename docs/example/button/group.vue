e4ab8f1228f26d02b18d1e5ca89046f03f797267
wzc520pyfm <1528857653@qq.com>
2023-04-04 20:46:24 +0800

    docs: update button group doc

---
+++ docs/example/button/group.vue
@@ -0,0 +1,51 @@
+<template>
+  <div space-y-2>
+    <div fscw gap-2>
+      <o-button-group>
+        <o-button type="primary">
+          Previous Page
+          <template #icon>
+            <div i-carbon-chevron-left />
+          </template>
+        </o-button>
+        <o-button type="secondary" class="align-bottom">
+          Next Page <div i-carbon-chevron-right />
+        </o-button>
+      </o-button-group>
+      <o-button-group>
+        <OButton>
+          <template #icon>
+            <div i-carbon-asleep />
+          </template>
+        </OButton>
+        <OButton>
+          <template #icon>
+            <div i-carbon-basketball />
+          </template>
+        </OButton>
+        <OButton>
+          <template #icon>
+            <div i-carbon-ai-status-rejected />
+          </template>
+        </OButton>
+      </o-button-group>
+      <o-button-group spacer>
+        <OButton rounded-full type="primary">
+          <template #icon>
+            <div i-carbon-asleep />
+          </template>
+        </OButton>
+        <OButton rounded-full type="primary">
+          <template #icon>
+            <div i-carbon-basketball />
+          </template>
+        </OButton>
+        <OButton rounded-full type="primary">
+          <template #icon>
+            <div i-carbon-ai-status-rejected />
+          </template>
+        </OButton>
+      </o-button-group>
+    </div>
+  </div>
+</template>
