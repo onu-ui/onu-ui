5b3b94ba4d44ad7ff82d033274d918cd05709764
yzh990918 <251205668@qq.com>
2023-05-11 19:59:42 +0800

    style: add overview-card hover border

--- docs/unocss.config.ts
+++ docs/unocss.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
       'o-demo_action_icon': 'c-#4E5969 dark:c-white',
       'o-demo_tooltip':
         'opacity-0 h-7 bg-black px-2.5 py-1 box-border text-xs c-white inline-flex justify-center items-center  rounded absolute z-1 transition duration-600 whitespace-nowrap -top-8',
-      'o-overview_card_wrapper': 'flex flex-col  items-center space-x-3 p-4 rounded border border-light-500 hover:bg-#F2F2F2 hover:-translate-y-2 !duration-400 dark:border-#4C4D4F dark:hover:bg-dark-300 !transition-all',
+      'o-overview_card_wrapper': 'flex flex-col  items-center space-x-3 p-4 rounded border border-light-500 hover:bg-#F2F2F2 hover:-translate-y-2 !duration-400 dark:border-#4C4D4F dark:hover:bg-dark-300 !hover-b-emerald-300 !transition-all',
       'o-overview_cover': 'children-w-full children-aspect-video',
       'o-overview_body': 'flex p-4 !text-base flex-1 flex-col gap-2',
       'overview_body_title': 'mb-2 flex items-center gap-2 !text-lg font-bold',
