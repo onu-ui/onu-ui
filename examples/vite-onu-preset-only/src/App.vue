6d970426a0f10965d13bc568ad66902ed6629045
Chris <hizyyv@gmail.com>
2023-11-01 23:33:22 +0800

    feat(preset): button-group component

--- examples/vite-onu-preset-only/src/App.vue
+++ examples/vite-onu-preset-only/src/App.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue'
 
 const value = ref('')
 const disabled = ref(true)
+const count = ref(4)
+const index = ref(0)
 
 document.body.classList.add('dark')
 
@@ -53,6 +55,13 @@ function toggleDisabled() {
       <input v-model="value" type="text" :disabled="disabled" class="input" placeholder="input...">
       <input v-model="value" type="text" class="input input-sm" placeholder="input input-sm...">
       <input v-model="value" type="text" :disabled="disabled" class="input input-none" placeholder="input input-none...">
+    </div>
+    <div fcc gap-2>
+      <div class="btn-group">
+        <button v-for="i in count" :key="i" class="btn" :class="index === i - 1 ? 'btn-active' : ''" @click="index = i - 1">
+          Button
+        </button>
+      </div>
     </div>
   </div>
 </template>
--- packages/preset/src/shortcuts/button-group.ts
+++ packages/preset/src/shortcuts/button-group.ts
@@ -1,5 +1,9 @@
 export const buttonGroupShortcuts: Record<string, string> = {
-  // button group
-  'o-button-group-clearfix': 'o-after:psdc-DEFAULT after:clear-both after:table o-before:psdc-DEFAULT before:table',
-  'o-button-group-children': 'o-children:[.o-button-base]:left o-children:[.o-button-base]:relative',
+  'btn-group': `
+        flex items-center justify-center gap-0
+        [&>.btn]:(rounded-0 bg-[rgb(var(--color-gray-300))])
+        dark:[&>.btn]:(bg-[rgb(var(--color-gray-700))])
+        first:[&>.btn]:(rounded-l-md) last:[&>.btn]:(rounded-r-md)
+        [&>.btn-active]:bg-[rgb(var(--color-primary-DEFAULT))]!
+    `,
 }
