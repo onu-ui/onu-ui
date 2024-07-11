86e9edd74a39fbea62e90ad5d467f0429527c03e
Chris <hizyyv@gmail.com>
2024-07-11 18:17:58 +0800

    chore: update

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import Components from 'unplugin-vue-components/vite'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 export default defineConfig({
   optimizeDeps: {
@@ -26,4 +27,9 @@ export default defineConfig({
       ],
     }),
   ],
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+  },
 })
