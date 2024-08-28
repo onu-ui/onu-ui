c4c3f6239db63500d5322d42837ab1f607f8b31a
Chris <hizyyv@gmail.com>
2024-08-28 17:13:05 +0800

    test: update

--- test/presets/index.test.ts
+++ test/presets/index.test.ts
@@ -20,45 +20,4 @@ describe('onu preset', () => {
     const { css } = await uno.generate('')
     expect(await formatCSSCode(css)).toMatchSnapshot()
   })
-
-  it('with prefix', async () => {
-    const uno = createOnu({ prefix: 'n-' })
-
-    const { css } = await uno.generate(`
-      <div class="n-btn n-btn-red n-o-red">Hello</div>
-    `, { preflights: false })
-
-    expect(await formatCSSCode(css)).toMatchInlineSnapshot(`
-      "/* layer: default */
-      .n-btn-red,
-      .n-o-red {
-        --onu-color-context: 0 91 71;
-      }
-      .n-btn {
-        --onu-color-context: var(--onu-color-500);
-        --onu-color-context: var(--onu-color-text);
-        border-width: 0px;
-        --un-border-opacity: 1;
-        border-color: hsl(var(--onu-color-context) / var(--un-border-opacity));
-        --un-bg-opacity: 1;
-        background-color: hsl(var(--onu-color-context) / var(--un-bg-opacity));
-      }
-      .n-btn:disabled {
-        opacity: 0.64;
-      }
-      "
-    `)
-
-    const { css: noncss } = await uno.generate(`
-      <div class="btn btn-red rounded">Hello</div>
-    `, { preflights: false })
-
-    expect(await formatCSSCode(noncss)).toMatchInlineSnapshot(`
-      "/* layer: default */
-      .rounded {
-        border-radius: 0.25rem;
-      }
-      "
-    `)
-  })
 })
