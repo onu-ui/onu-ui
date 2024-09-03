5d38bea46e5c65afe67cf0cc0d0b4beb92ea08f2
Chris <hizyyv@gmail.com>
2024-09-03 18:12:42 +0800

    fix(preset): remove unused deps (#216)
    * fix(preset): remove unused deps
    
    * chore: update
    
    * chore: update
    
    * chore: update
    
    * chore: update

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   external: ['vue'],
   dts: true,
   minify: true,
