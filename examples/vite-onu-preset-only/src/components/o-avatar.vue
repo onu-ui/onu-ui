76c4f917522955ee64208d123cbaa9f6457594df
Chris <hizyyv@gmail.com>
2024-11-11 22:03:58 +0800

    chore: update

--- examples/vite-onu-preset-only/src/components/o-avatar.vue
+++ examples/vite-onu-preset-only/src/components/o-avatar.vue
@@ -2,44 +2,44 @@
   <OCard title="Avatar Component" desc="Avatar component with custom style">
     <div flex="~ col gap-2">
       <div flex items-center justify-center gap-2>
-        <div class="avatar">
+        <div class="avatar hover:animate-rubber-band">
           <div class="rounded">
             <img src="https://avatars.githubusercontent.com/u/42139754">
           </div>
         </div>
-        <div class="avatar avatar-lg">
+        <div class="avatar avatar-lg hover:animate-rubber-band">
           <div class="rounded">
             <img src="https://avatars.githubusercontent.com/u/42139754">
           </div>
         </div>
-        <div class="avatar avatar-md">
+        <div class="avatar avatar-md hover:animate-rubber-band">
           <div class="rounded">
             <img src="https://avatars.githubusercontent.com/u/42139754">
           </div>
         </div>
-        <div class="avatar avatar-sm">
+        <div class="avatar avatar-sm hover:animate-rubber-band">
           <div class="rounded">
             <img src="https://avatars.githubusercontent.com/u/42139754">
           </div>
         </div>
-        <div class="avatar avatar-xs">
+        <div class="avatar avatar-xs hover:animate-rubber-band">
           <div class="rounded">
             <img src="https://avatars.githubusercontent.com/u/42139754">
           </div>
         </div>
       </div>
       <div flex items-center justify-center gap-2>
-        <div class="avatar mask-hexagon">
+        <div class="avatar mask-hexagon hover:animate-rubber-band">
           <div class="w-24 rounded">
             <img src="https://avatars.githubusercontent.com/u/42139754">
           </div>
         </div>
-        <div class="avatar mask-squircle">
+        <div class="avatar mask-squircle hover:animate-rubber-band">
           <div class="w-24 rounded">
             <img src="https://avatars.githubusercontent.com/u/42139754">
           </div>
         </div>
-        <div class="avatar mask-triangle">
+        <div class="avatar mask-triangle hover:animate-rubber-band">
           <div class="w-24 rounded">
             <img src="https://avatars.githubusercontent.com/u/42139754">
           </div>
@@ -47,22 +47,22 @@
       </div>
       <div flex items-center justify-center gap-2>
         <div class="avatar-group avatar-group-sm space-x--6">
-          <div class="avatar">
+          <div class="avatar hover:animate-rubber-band">
             <div class="">
               <img src="https://avatars.githubusercontent.com/u/42139754">
             </div>
           </div>
-          <div class="avatar">
+          <div class="avatar hover:animate-rubber-band">
             <div class="">
               <img src="https://avatars.githubusercontent.com/u/42139754">
             </div>
           </div>
-          <div class="avatar">
+          <div class="avatar hover:animate-rubber-band">
             <div class="">
               <img src="https://avatars.githubusercontent.com/u/42139754">
             </div>
           </div>
-          <div class="avatar avatar-placeholder">
+          <div class="avatar hover:animate-rubber-band avatar-placeholder">
             <div class="">
               <span>+99</span>
             </div>
